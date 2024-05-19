ce9fc74794fa90145880c17de48c1217554147ea
john <johnnacpil01@gmail.com>
2024-05-19 16:40:59 +0800

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,49 @@
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}" class="row g-3 needs-validation">
+        @csrf
+
+        <!-- Email Address -->
+        <div class="col-12">
+            <x-input-label for="email" :value="__('Email')" class="form-label" />
+            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="invalid-feedback" />
+        </div>
+
+        <!-- Password -->
+        <div class="col-12">
+            <x-input-label for="password" :value="__('Password')" class="form-label" />
+
+            <x-text-input id="password" class="form-control"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="invalid-feedback" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input"name="remember" id="remember_me" type="checkbox">
+                <label class="form-check-label" for="remember_me">{{ __('Remember me') }}</label>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <x-primary-button class="btn btn-primary w-100">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+        </div>
+    </form>
+</x-guest-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,55 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name"  class="form-control"  type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="invalid-feedback" />
+        </div>
+
+       
+        <!-- Email Address -->
+        <div class="col-12">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="invalid-feedback" />
+        </div>
+
+        <!-- Password -->
+        <div class="col-12">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="form-control"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="invalid-feedback" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="col-12">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="form-control"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="invalid-feedback" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+            <div class="col-12">
+                <x-primary-button class="btn btn-primary w-100">
+                    {{ __('Register') }}
+                </x-primary-button>
+            </div>
+          
+        </div>
+    </form>
+</x-guest-layout>
