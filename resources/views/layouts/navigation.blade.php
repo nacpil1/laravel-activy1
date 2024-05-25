19524138ead0f35001f4b3148a8893715e0091f8
john <johnnacpil01@gmail.com>
2024-05-25 21:00:20 +0800

    Feature/eloquent-relationship done

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,22 +1,31 @@
 <aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('dashboard') }}">
+            <a class="nav-link {{ request()->routeIs('dashboard') ? 'active' : '' }}" href="{{ route('dashboard') }}">
                 <i class="bi bi-grid"></i>
                 <span>{{ __('Dashboard') }}</span>
             </a>
         </li>
 
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#resources-nav" data-bs-toggle="collapse" href="#"> <i class="bi bi-menu-button-wide"></i><span>Resources</span><i class="bi bi-chevron-down ms-auto"></i> </a>
-            <ul id="resources-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+            <a class="nav-link {{ request()->routeIs('post.index') ? 'active' : 'collapsed' }}" data-bs-target="#resources-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-menu-button-wide"></i>
+                <span>{{ __('Resources') }}</span>
+                <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="resources-nav" class="nav-content collapse {{ request()->routeIs('post.index') ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
                 <li>
-                    <a href="{{ route('post.index') }}"> <i class="bi bi-circle"></i><span>Posts</span> </a>
+                    <a class="{{ request()->routeIs('post.index') ? 'active' : '' }}" href="{{ route('post.index') }}">
+                        <i class="bi bi-circle"></i>
+                        <span>{{ __('Posts') }}</span>
+                    </a>
                 </li>
             </ul>
         </li>
     </ul>
 </aside>
+
+
 
 {{-- <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
